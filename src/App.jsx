d2abbe0cbe22382677e0950f8aee7b867dfa9c76
Yahya Al-hamedi <y.hamedi@jisr.net>
2025-05-22 18:32:00 +0300

    Add config files to the project [eslint, perttier]

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 function App() {
-  return (
-    <h1>React Investment Calculator</h1>
-  )
+  return <h1>React Investment Calculator</h1>
 }
 
 export default App
--- src/util/investment.js
+++ src/util/investment.js
@@ -10,21 +10,21 @@ export function calculateInvestmentResults({
   expectedReturn,
   duration,
 }) {
-  const annualData = [];
-  let investmentValue = initialInvestment;
+  const annualData = []
+  let investmentValue = initialInvestment
 
   for (let i = 0; i < duration; i++) {
-    const interestEarnedInYear = investmentValue * (expectedReturn / 100);
-    investmentValue += interestEarnedInYear + annualInvestment;
+    const interestEarnedInYear = investmentValue * (expectedReturn / 100)
+    investmentValue += interestEarnedInYear + annualInvestment
     annualData.push({
       year: i + 1, // year identifier
       interest: interestEarnedInYear, // the amount of interest earned in this year
       valueEndOfYear: investmentValue, // investment value at end of year
       annualInvestment: annualInvestment, // investment added in this year
-    });
+    })
   }
 
-  return annualData;
+  return annualData
 }
 
 // The browser-provided Intl API is used to prepare a formatter object
@@ -35,4 +35,4 @@ export const formatter = new Intl.NumberFormat('en-US', {
   currency: 'USD',
   minimumFractionDigits: 0,
   maximumFractionDigits: 0,
-});
+})
