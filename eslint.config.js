404f777676618e631d6a20212004a3c943b354b9
Yahya Al-hamedi <y.hamedi@jisr.net>
2025-05-24 13:54:39 +0300

    Lifting the state up in the upper component

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,7 @@ import pluginReact from 'eslint-plugin-react'
 import { defineConfig } from 'eslint/config'
 
 export default defineConfig([
-  { files: ['**/*.{js,mjs,cjs,jsx}'], plugins: { js }, extends: ['js/recommended'] },
+  { files: ['**/*.{js,mjs,cjs,jsx,ts,tsx}'], plugins: { js }, extends: ['js/recommended'] },
   { files: ['**/*.{js,mjs,cjs,jsx}'], languageOptions: { globals: globals.browser } },
   pluginReact.configs.flat.recommended,
   {
--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
+import { useState } from 'react'
 import Header from './components/Header'
 import UserInput from './components/UserInput'
 
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  })
+
+  const handleChange = (inputIdentifier, newValue) => {
+    setUserInput((prevState) => {
+      return {
+        ...prevState,
+        [inputIdentifier]: newValue,
+      }
+    })
+  }
+
   return (
     <>
       <Header />
-      <UserInput />
+      <UserInput
+        handleChange={handleChange}
+        userInput={userInput}
+      />
     </>
   )
 }
